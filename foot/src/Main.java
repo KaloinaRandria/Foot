376846a89541abb6f2e1ffe50b73db5bf1817c64
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-01-14 19:34:47 +0300

    test swing

--- foot/src/Main.java
+++ foot/src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+import affichage.Graph;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // Lancement de l'application Swing dans le thread principal
+        SwingUtilities.invokeLater(Graph::new);
     }
 }
